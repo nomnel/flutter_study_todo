3038bba56747f70e48f869e81228e25817d80e7f
nomnel <laxyz.nomnel@gmail.com>
2020-07-26 20:36:24 +0900

    as ToDo App

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context) => CountData(),
+      create: (context) => TodoData(),
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        home: MyHomePage(title: 'Flutter Demo Home Page'),
+        home: MyHomePage(title: 'todo app'),
       ),
     );
   }
@@ -29,26 +29,49 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var data = Provider.of<TodoData>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: ListView.builder(
+        itemCount: data.length(),
+        itemBuilder: (context, index) {
+          var todo = data.get(index);
+          return ListTile(
+            leading: GestureDetector(
+              child: todo.isDone
+                  ? Icon(Icons.check_box)
+                  : Icon(Icons.check_box_outline_blank),
+              onTap: () {
+                data.toggleStatus(index);
+              },
             ),
-            Text(
-              Provider.of<CountData>(context).count.toString(),
-              style: Theme.of(context).textTheme.headline4,
+            title: data.isActive(index)
+                ? TextField(
+                    controller: TextEditingController(text: todo.body),
+                    onEditingComplete: data.inactivate,
+                    onSubmitted: (value) {
+                      data.update(index, value);
+                    },
+                  )
+                : GestureDetector(
+                    child: Text(todo.body),
+                    onTap: () {
+                      data.activate(index);
+                    },
+                  ),
+            trailing: GestureDetector(
+              child: Icon(Icons.delete),
+              onTap: () {
+                data.remove(index);
+              },
             ),
-          ],
-        ),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: Provider.of<CountData>(context, listen: false).increment,
+        onPressed: data.add,
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ),
@@ -56,11 +79,57 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
-class CountData extends ChangeNotifier {
-  int count = 0;
+class TodoData extends ChangeNotifier {
+  int _activeIndex = -1;
+  List<Todo> _todoList = [];
 
-  void increment() {
-    count += 1;
+  void activate(int index) {
+    _activeIndex = index;
     notifyListeners();
+  }
+
+  void add() {
+    _todoList.insert(0, Todo(''));
+    activate(0);
+  }
+
+  List<Todo> all() => _todoList;
+
+  Todo get(int index) => _todoList[index];
+
+  void inactivate() {
+    _activeIndex = -1;
+    notifyListeners();
+  }
+
+  bool isActive(int index) => index == _activeIndex;
+
+  int length() => _todoList.length;
+
+  void remove(int index) {
+    _todoList.removeAt(index);
+    inactivate();
+  }
+
+  void toggleStatus(int index) {
+    var todo = _todoList[index];
+    todo.isDone = !todo.isDone;
+    inactivate();
+  }
+
+  void update(int index, String body) {
+    _todoList[index].body = body;
+    inactivate();
+  }
+}
+
+class Todo {
+  String body;
+  bool isDone;
+  DateTime createdAt;
+
+  Todo(this.body) {
+    this.isDone = false;
+    this.createdAt = DateTime.now();
   }
 }
